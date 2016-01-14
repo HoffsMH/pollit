1d0780626100cca547fcbb77d17b32cda36e8367
HoffsMH <hecker.matthew@gmail.com>
2016-01-14 11:09:19 -0700

    testing setup done for now

--- test/server-test.js
+++ test/server-test.js
@@ -1,11 +1,11 @@
 const assert = require('assert');
 const expect = require('chai').expect;
-const app = require('../app');
+const server = require('../server');
 
 describe('Server', () => {
 
   it('should exist', () => {
-    expect(app).to.not.be.false
+    expect(server).to.not.be.false
   });
 
 });
