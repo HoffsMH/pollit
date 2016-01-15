331065bb770d03ee9a01858a4a0abfa7497d6215
HoffsMH <hecker.matthew@gmail.com>
2016-01-15 12:25:51 -0700

    added more form inputs

---
+++ app/models/poll.js
@@ -0,0 +1,24 @@
+// function Poll() {
+//
+//
+// }
+//
+// Poll.prototype
+//
+//
+// {
+//   id: "asdfa",
+//   adminId: "asdflasdf",
+//   open: true,
+//   question: "who let the dogs out",
+//   choices: {
+//     "who": 1,
+//     "who3": 5,
+//     "who2": 3,
+//     "who1": 2,
+//   },
+//   createdAt:
+//
+// }
+
+module.exports = {};
